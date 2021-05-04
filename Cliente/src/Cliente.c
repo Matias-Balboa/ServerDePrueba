aab08698d7360c3ea324bfbf7a59993ba34f4a71
Matias Balboa <matiasbp7@hotmail.com>
2021-05-04 20:35:05 -0300

    cliente terminado

--- Cliente/src/Cliente.c
+++ Cliente/src/Cliente.c
@@ -10,8 +10,32 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
 
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
-	return EXIT_SUCCESS;
+
+	struct sockaddr_in direccionServidor;
+
+	direccionServidor.sin_family = AF_INET;
+	direccionServidor.sin_addr.s_addr //Buscar ip
+	direccionServidor.sin_port = htons(8080);
+
+	int cliente = socket(direccionServidor->sin_family,SOCK_STREAM,0);
+
+	if(conect(cliente,(void*)&direccionServidor,sizeof(direccionServidor))!=0){
+
+		perror("Error al conectar");
+		return 1;
+	}
+
+	while(1){
+		char mensaje[1000];
+		scanf("%s",mensaje);
+		send(cliente,mensaje,strlen(mensaje),0);
+	}
+
+	return 0;
 }
