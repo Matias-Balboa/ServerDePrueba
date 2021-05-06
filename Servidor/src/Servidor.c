2c7f02378846e1fecffadba43a7a596c6aee4df2
Matias Balboa <matiasbp7@hotmail.com>
2021-05-06 18:12:06 -0300

    arreglos

--- Servidor/src/Servidor.c
+++ Servidor/src/Servidor.c
@@ -35,7 +35,7 @@ int main(void) {
 	int activado = 1; // True
 	// Setea el flag activado y con SO_REUSEADOR le indica al SO que le permita
 	// reutilizar las direcciones.
-	setsockopt(servidor, SOL_SOCKET, SO_REUSEADDR, %activado, sizeof(activado));
+	setsockopt(servidor, SOL_SOCKET, SO_REUSEADDR, &activado, sizeof(activado));
 
 	/* Asociamos con la funcion bind el socket con la direccion del servidor.
 	  bind(socket, puntero a la direccion del servidor, el tamanio del servidor);
@@ -51,7 +51,7 @@ int main(void) {
 	/* Esta funcion nos permite escuchar, le pasamos el servidor creado y un numero que
 	   es el maximo de conexiones acumulables ya que toda proxima conexion entrante
 	   se va a cumular en una cola hasta que nosotros la aceptemos. */
-	listen(servidor, 100);
+	listen(servidor, SOMAXCONN);
 	// listen(servidor, SOMAXCONN);
 	/* Si creo que voy a tener mucha concurrencia con SOMAXCONN me
 	   aseguro que el tamanio de la cola va a ser el maximo. */
@@ -67,34 +67,36 @@ int main(void) {
 	   cliente que se conecto */
 	int cliente = accept(servidor, (void*) &direccionCliente, &tamanioDireccionCliente);
 
-	printf("Recibi una conexion en %d!\n :O", cliente);
+	printf("Recibi una conexion en: %d \n", cliente);
 
 	// Le enviamos un mensaje al cliente mediante la funcion send.
 	// send(socket del cliente, buffer, tamanio del buffer o cant bytes, flags);
-	send(cliente, "Hola! Soy el Servidor :)\n", 30, 0);
+	//send(cliente, "Hola! Soy el Servidor :)\n", 30, 0);
 
 	// -------------- RECIBIMOS DATOS DEL CLIENTE -------------- //
 
 	// Creo un buffer de 5 bytes para almacenar el mensaje enviado por el cliente
-	char* buffer = malloc(5);
+	char* buffer = malloc(1000);
 	/* Con la funcion recv le decimos que de ese socket el cliente, lo que recibamos
 	   guardalo en el buffer y que quiero recibir como maximo 4 bytes. Ademas, quiero
 	   que respetes el comportamiento predeterminado asi que no le pasamos ningun flag.
 	   recv(socket del cliente, donde almacenamos lo que recibimos, cant maxima de bytes, flag);
 	   El resultado de la funcion recv son los bytes recibidos.
 	   Si recibimos mas bytes de los que indicamos los tenemos que recibir dsp. */
-	int bytesRecibidos = recv(cliente, buffer, 4, 0);
-
-	if(bytesRecibidos < 0) {
-		perror("Se desconecto el cliente! u.u\n");
-		return 1;
-	}
-
+		//int bytesRecibidos = recv(cliente, buffer, 1000, 0);
+	while(1){
+		int bytesRecibidos = recv(cliente, buffer, 1000, 0);
+		if(bytesRecibidos < 0) {
+			printf("Los bytes:%d \n",bytesRecibidos);
+			perror("Se desconecto el cliente! u.u\n");
+			return 1;
+		}
 	//En el buffer recibido, en el ultimo byte ya que recibimos un string y de esta
 	// forma podemos hacer un printf.
 	buffer[bytesRecibidos] = '\0';
 
 	printf("Me llegaron %d bytes con %s", bytesRecibidos, buffer);
+	}
 
 	// Liberamos el buffer.
 	free(buffer);
