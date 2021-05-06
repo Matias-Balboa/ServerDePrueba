e43dcec91a841f8372354d056d9d999c0ebe3d76
Antonella Bevilacqua <abevilacqua@frba.utn.edu.ar>
2021-05-06 15:29:16 -0300

    Subo parte del servidor

--- Servidor/src/Servidor.c
+++ Servidor/src/Servidor.c
@@ -10,8 +10,27 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
 
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
-	return EXIT_SUCCESS;
+	struct sockaddr_in direccionServidor;
+	direccionServidor.sin_family = AF_INET;
+	direccionServidor.sin_addr.s_addr = INADDR_ANY;
+	direccionServidor.sin_port = htons(8080);
+
+	int servidor = socket(AF_INET, SOCK_STREAM, 0);
+
+	if(bind(servidor, (void*) &direccionServidor, sizeof(direccionServidor)) != 0) {
+		perror("Fallo el bind!");
+		return 1;
+	}
+
+	printf("Ya estoy escuchando :)\n");
+	listen(servidor, 100);
+
+	for( ; ; );
+
+	return 0;
+
 }
